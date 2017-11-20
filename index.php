e81b332f13d6052df645520a3a5b616af01c21a5
rafaelrenck <rafaelrenck@gmail.com>
2017-11-20 15:58:30 -0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/foundation.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+	<title>Amazonia Book Store</title>
+</head>
+<body>
+	<div class="grid-container fluid bggreen">
+		<div class="grid-container">
+			<div class="grid-x grid-padding-x">
+				<div class="medium-shrink cell">
+					<img src="img/logo.png">
+				</div>
+				<div class="auto cell">
+					<form class="formsearch mtop20 right">
+						<div class="grid-x grid-padding-x small-padding-collapse pright15 pleft15">
+							<div class="auto cell">
+								<input type="text" name="search" placeholder="Pesquisar..">
+							</div>
+							<div class="shrink cell">
+								<input type="submit" name="consultar" value="&#57347;" class="button icon">
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="shrink cell">
+					<div class="grid-x grid-padding-x small-padding-collapse pright15 pleft15">
+						<div class="shrink cell">
+							<span class="icon green big h80">&#57352;</span>
+						</div>
+						<div class="shrink cell">
+							<span class="green h80 pleft5">Minha Conta</span>
+						</div>
+					</div>
+				</div>
+				<div class="shrink cell">
+					<span class="icon green big h80">&#57622;</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="grid-container mtop20">
+		<div class="grid-x grid-padding-x">
+			<div class="small-4 medium-3 cell">
+				<div class="grid-x grid-padding-x">
+					<div class="cell">
+						<h1>Categorias</h1>
+						<ul>
+							<li>Administração</li>
+							<li>Artes</li>
+							<li>Biografia</li>
+							<li>...</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="small-8 medium-9 cell">
+				<div class="grid-x grid-padding-x">
+					<div class="cell center">
+						<h1>Destaques</h1>
+					</div>
+					<div class="medium-6 large-4 cell">
+						1
+					</div>
+					<div class="medium-6 large-4 cell">
+						2
+					</div>
+					<div class="medium-6 large-4 cell">
+						3
+					</div>
+					<div class="medium-6 large-4 cell">
+						1
+					</div>
+					<div class="medium-6 large-4 cell">
+						2
+					</div>
+					<div class="medium-6 large-4 cell">
+						3
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
