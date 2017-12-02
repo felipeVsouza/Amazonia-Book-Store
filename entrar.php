569fcaad053a823c3bec817c93bf57e934d31adb
fvsgamers <felipe.vieira808@gmail.com>
2017-12-01 22:22:25 -0200

    entrada para admin ou cliente 'entrar.php'

--- entrar.php
+++ entrar.php
@@ -1,15 +1,25 @@
 <?php
 	session_start();
 	include_once("conn.php");
-	$result = $conn->query("SELECT * FROM cliente WHERE email = '".$_POST['email']."' AND senha = '".$_POST['senha']."'");
-	$row = $result->fetch_assoc();
-	if ($result->num_rows >= 1) {
+	$result1 = $conn->query("SELECT * FROM cliente WHERE email = '".$_POST['email']."' AND senha = '".$_POST['senha']."'");
+	$result2 = $conn->query("SELECT * FROM usuario WHERE login = 'rafael' AND senha = 'rafael'");
+	$row1 = $result1->fetch_assoc();
+	$row2 = $result2->fetch_assoc();
+	if ($result1->num_rows >= 1) {
 		$_SESSION['id_cliente'] = $row['id_cliente'];
-		$_SESSION['cliente'] = $row['nome'];
+		$_SESSION['cliente'] = $row1['nome'];
 		$_SESSION['email'] = $_POST['email'];
 		$_SESSION['senha'] = $_POST['senha'];
 		header("Location: index.php");
-	} else {
-		header("Location: index.php?login&erro=1");
-	}
+	} else if ($result2->num_rows >= 1){
+		$_SESSION['id_usuario'] = $row2['id_usuario'];
+		$_SESSION['login'] = $row['login'];
+		$_SESSION['login'] = $_POST['login'];
+		$_SESSION['senha'] = $_POST['senha'];
+		header("Location: cadastros.php");
+		}	
+		else{
+				header("Location: index.php?login&erro=1");
+			}
+	
 ?>
