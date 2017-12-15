9be1e562d133b9ddb98a9c2ff6fc59c9a5fd7708
fvsgamers <felipe.vieira808@gmail.com>
2017-12-15 20:30:18 -0200

    crud usuario

--- admin/usuario.php
+++ admin/usuario.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Amazonia Book Store | Admin | Cadastros | Clientes</title>
+	<title>Amazonia Book Store | Admin | Cadastros | usuarios</title>
 	<link rel="stylesheet" type="text/css" href="../css/foundation.css">
 </head>
 <body>
@@ -11,50 +11,50 @@
 	include_once('../conn.php');
 	if (isset($_SESSION['usuario'])) {
 		if (isset($_GET['editar'])) {
-			$result = $conn->query("SELECT * FROM cliente WHERE id_cliente= '".$_GET['editar']."'");
+			$result = $conn->query("SELECT * FROM usuario WHERE id_usuario= '".$_GET['editar']."'");
 			$row = $result->fetch_assoc();
 			echo "
 				<form method='POST' action='usuario.php'>
-					<label>cliente:</label>
+					<label>usuario:</label>
 					<input type='text' name='usuario' value='".utf8_encode($row['usuario'])."'>
-					<input type='hidden' name='id_cliente' value='".$row['id_cliente']."'>
+					<input type='hidden' name='id_usuario' value='".$row['id_usuario']."'>
 					<input type='submit' name='atualizar' value='Atualizar'>
 				</form>
 			";
 		} else if (isset($_GET['excluir'])) {
-			$result = $conn->query("SELECT * FROM cliente WHERE id_cliente= '".$_GET['excluir']."'");
+			$result = $conn->query("SELECT * FROM usuario WHERE id_usuario= '".$_GET['excluir']."'");
 			$row = $result->fetch_assoc();
 			echo "
 				<form method='POST' action='usuario.php'>
-					<label>cliente:</label>
+					<label>usuario:</label>
 					<input type='text' name='usuario' value='".utf8_encode($row['usuario'])."'>
-					<input type='hidden' name='id_cliente' value='".$row['id_cliente']."'>
+					<input type='hidden' name='id_usuario' value='".$row['id_usuario']."'>
 					<input type='submit' name='excluir' value='Confirma exclusão'>
 				</form>
 			";
 		} else {
 			echo "
 				<form method='POST' action='usuario.php'>
-					<label>cliente:</label>
+					<label>usuario:</label>
 					<input type='text' name='usuario'>
 					<input type='submit' name='cadastrar' value='Cadastrar'>
 				</form>
 			";
 		}
 		if (isset($_POST['cadastrar'])) {
-			$result = $conn->query("INSERT INTO cliente(nome) VALUES('".$_POST['usuario']."')");
+			$result = $conn->query("INSERT INTO usuario(nome) VALUES('".$_POST['usuario']."')");
 
 		}
 		if (isset($_POST['atualizar'])) {
-			$conn->query("UPDATE cliente SET nome = '".$_POST['usuario']."' WHERE id_cliente = '".$_POST['id_cliente']."'");
+			$conn->query("UPDATE usuario SET nome = '".$_POST['usuario']."' WHERE id_usuario = '".$_POST['id_usuario']."'");
 		}
 		if (isset($_POST['excluir'])) {
-			$conn->query("DELETE FROM cliente WHERE id_cliente = '".$_POST['id_cliente']."'");
+			$conn->query("DELETE FROM usuario WHERE id_usuario = '".$_POST['id_usuario']."'");
 		}
-		$result = $conn->query("SELECT * FROM cliente ORDER BY nome");
-		echo "<table><tr><th>ID</th><th>cliente</th><th colspan='2'>Ações</tr>";
+		$result = $conn->query("SELECT * FROM usuario ORDER BY nome");
+		echo "<table><tr><th>ID</th><th>usuario</th><th colspan='2'>Ações</tr>";
 		while ($row = $result->fetch_assoc()) {
-			echo "<tr><td>".$row['id_cliente']."</td><td>".utf8_encode($row['usuario'])."</td><td><a href='usuario.php?editar=".$row['id_cliente']."'>Atualizar</a></td><td><a href='usuario.php?excluir=".$row['id_cliente']."'>Excluir</a></td></tr>";
+			echo "<tr><td>".$row['id_usuario']."</td><td>".utf8_encode($row['usuario'])."</td><td><a href='usuario.php?editar=".$row['id_usuario']."'>Atualizar</a></td><td><a href='usuario.php?excluir=".$row['id_usuario']."'>Excluir</a></td></tr>";
 		}
 		echo "</table>";
 	} else {
